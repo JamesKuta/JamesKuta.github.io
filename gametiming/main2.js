09f90652efc7a32178f547b9518666b9cf64fad4
JamesKuta <38857849+JamesKuta@users.noreply.github.com>
2018-10-28 10:46:29 -0500

    Add files via upload

--- gametiming/main2.js
+++ gametiming/main2.js
@@ -13,7 +13,7 @@ let resize = function () {
 window.onresize = resize;
 resize()
 
-let state = {
+let ship = {
   position: {
     x: (width / 2),
     y: (height / 2)
@@ -40,10 +40,10 @@ function Bullets(xPos, yPos, initialSpeedX, initialSpeedY) {
   this.initialSpeedY = initialSpeedY;
 }
 
-function createBullets() {
-  if(state.pressedKeys.space) {
-    bulletArray.push(new Bullets(state.position.x, state.position.y, state.movement.x, state.movement.y));
-    state.pressedKeys.space = false;
+function createBullets(p) {
+    if(ship.pressedKeys.space) {
+    bulletArray.push(new Bullets(ship.position.x, ship.position.y, p * 50 * Math.cos((ship.rotation - 90) * (Math.PI / 180)), p * 50 * Math.sin((ship.rotation - 90) * (Math.PI / 180))));
+    ship.pressedKeys.space = false;
   }
 }
 
@@ -53,205 +53,210 @@ function update(progress) {
   updateRotation(p)
   updateMovement(p)
   updatePosition(p)
-  createBullets()
+  createBullets(p)
 }
 
 
 function updateRotation(p) {
-  if (state.pressedKeys.left) {
-    state.rotation -= p * 5;
+  if (ship.pressedKeys.left) {
+    ship.rotation -= p * 5;
   }
-  else if (state.pressedKeys.right) {
-    state.rotation += p * 5;
+  else if (ship.pressedKeys.right) {
+    ship.rotation += p * 5;
   }
 }
 
 function updateMovement(p) {
-  // Behold! Mathematics for mapping a rotation to it's x, y components
+  //Math for mapping a rotation to it's x, y
   let accelerationVector = {
-    x: p * 0.3 * Math.cos((state.rotation - 90) * (Math.PI / 180)),
-    y: p * 0.3 * Math.sin((state.rotation - 90) * (Math.PI / 180))
+    x: p * 0.3 * Math.cos((ship.rotation - 90) * (Math.PI / 180)),
+    y: p * 0.3 * Math.sin((ship.rotation - 90) * (Math.PI / 180))
   };
   //console.log(accelerationVector.x, ' ', accelerationVector.y);
 
-  if (state.pressedKeys.up) {
-    state.movement.x += accelerationVector.x;
-    state.movement.y += accelerationVector.y;
+  if (ship.pressedKeys.up) {
+    ship.movement.x += accelerationVector.x;
+    ship.movement.y += accelerationVector.y;
 
   }
-  else if (state.pressedKeys.down) {
-    state.movement.x -= accelerationVector.x;
-    state.movement.y -= accelerationVector.y;
+  else if (ship.pressedKeys.down) {
+    ship.movement.x -= accelerationVector.x;
+    ship.movement.y -= accelerationVector.y;
   }
 
   // Limit movement speed
-  if (state.movement.x > 40) {
-    state.movement.x = 40;
+  if (ship.movement.x > 40) {
+    ship.movement.x = 40;
   }
-  else if (state.movement.x < -40) {
-    state.movement.x = -40;
+  else if (ship.movement.x < -40) {
+    ship.movement.x = -40;
   }
-  if (state.movement.y > 40) {
-    state.movement.y = 40;
+  if (ship.movement.y > 40) {
+    ship.movement.y = 40;
   }
-  else if (state.movement.y < -40) {
-    state.movement.y = -40;
-  }
-
-  if (state.movement.y > 0) {
-    state.movement.y -= state.movement.friction;
+  else if (ship.movement.y < -40) {
+    ship.movement.y = -40;
   }
 
-  if (state.movement.y < 0) {
-    state.movement.y += state.movement.friction;
+  if (ship.movement.y > 0) {
+    ship.movement.y -= ship.movement.friction;
   }
 
-  if (state.movement.x > 0) {
-    state.movement.x -= state.movement.friction;
+  if (ship.movement.y < 0) {
+    ship.movement.y += ship.movement.friction;
   }
 
-  if (state.movement.x < 0) {
-    state.movement.x += state.movement.friction;
+  if (ship.movement.x > 0) {
+    ship.movement.x -= ship.movement.friction;
+  }
+
+  if (ship.movement.x < 0) {
+    ship.movement.x += ship.movement.friction;
   }
 
 }
 
 function updatePosition(p) {
-  state.position.x += state.movement.x;
-  state.position.y += state.movement.y;
+  ship.position.x += ship.movement.x;
+  ship.position.y += ship.movement.y;
 
   // Detect boundaries
-  if (state.position.x > width) {
-    state.position.x -= width;
+  if (ship.position.x > width) {
+    ship.position.x -= width;
   }
-  else if (state.position.x < 0) {
-    state.position.x += width;
+  else if (ship.position.x < 0) {
+    ship.position.x += width;
   }
-  if (state.position.y > height) {
-    state.position.y -= height;
+  if (ship.position.y > height) {
+    ship.position.y -= height;
   }
-  else if (state.position.y < 0) {
-    state.position.y += height;
+  else if (ship.position.y < 0) {
+    ship.position.y += height;
   }
 
   //update bullet positions
-  let accelerationVector = {
-    x: p * 10 * Math.cos((state.rotation - 90) * (Math.PI / 180)),
-    y: p * 10 * Math.sin((state.rotation - 90) * (Math.PI / 180))
-  };
+  // let accelerationVector = {
+  //   x: p * 10 * Math.cos((ship.rotation - 90) * (Math.PI / 180)),
+  //   y: p * 10 * Math.sin((ship.rotation - 90) * (Math.PI / 180))
+  // };
   for(let i=0; i < bulletArray.length; i++){
-    bulletArray[i].xPos; 
-    bulletArray[i].yPos;  
+    bulletArray[i].xPos += bulletArray[i].initialSpeedX; 
+    bulletArray[i].yPos += bulletArray[i].initialSpeedY;
   }
+
+  //destroy off screen bulletaaaaas
+
 }//end updatePostion()
 
 function draw() {
-  ctx.clearRect(0, 0, width, height)
+  ctx.clearRect(0, 0, width, height);
+  
+  drawBullets();
 
-  ctx.save()
+  ctx.save();
   //set the draw origin to be where the ship is
-  ctx.translate(state.position.x, state.position.y)
+  ctx.translate(ship.position.x, ship.position.y);
 
   //rotate the ship if needed
-  ctx.rotate((Math.PI / 180) * state.rotation)
+  ctx.rotate((Math.PI / 180) * ship.rotation);
   
 
   //draw ship body
   ctx.strokeStyle = 'white';
   ctx.lineWidth = 5;
-  ctx.beginPath()
+  ctx.beginPath();
   //right side of ship
-  ctx.moveTo(0, -40)
-  ctx.lineTo(5, -30)
-  ctx.lineTo(10, -20)
-  ctx.lineTo(5, -10)
-  ctx.lineTo(20, 0)
-  ctx.lineTo(25, 10)
-  ctx.lineTo(30, 20)
-  ctx.lineTo(35, 30)
-  ctx.lineTo(40, 40)
+  ctx.moveTo(0, -40);
+  ctx.lineTo(5, -30);
+  ctx.lineTo(10, -20);
+  ctx.lineTo(5, -10);
+  ctx.lineTo(20, 0);
+  ctx.lineTo(25, 10);
+  ctx.lineTo(30, 20);
+  ctx.lineTo(35, 30);
+  ctx.lineTo(40, 40);
 
   //bottom of ship
 
-  ctx.lineTo(20, 40)
-  ctx.lineTo(0, 20)
-  ctx.lineTo(-20, 40)
-  ctx.lineTo(-40, 40)
+  ctx.lineTo(20, 40);
+  ctx.lineTo(0, 20);
+  ctx.lineTo(-20, 40);
+  ctx.lineTo(-40, 40);
 
   // //left side of ship
-  ctx.lineTo(-35, 30)
-  ctx.lineTo(-30, 20)
-  ctx.lineTo(-25, 10)
-  ctx.lineTo(-20, 0)
-  ctx.lineTo(-5, -10)
-  ctx.lineTo(-10, -20)
+  ctx.lineTo(-35, 30);
+  ctx.lineTo(-30, 20);
+  ctx.lineTo(-25, 10);
+  ctx.lineTo(-20, 0);
+  ctx.lineTo(-5, -10);
+  ctx.lineTo(-10, -20);
 
 
 
-  ctx.closePath()
-  ctx.stroke()
+  ctx.closePath();
+  ctx.stroke();
 
   // draw the thrusters
-  if (state.pressedKeys.up) {
+  if (ship.pressedKeys.up) {
     ctx.strokeStyle = 'red';
-    ctx.beginPath()
-    ctx.moveTo(0, 20)
-    ctx.lineTo(0, 40)
-    ctx.moveTo(20, 40)
-    ctx.lineTo(20, 60)
-    ctx.moveTo(-20, 40)
-    ctx.lineTo(-20, 60)
-    ctx.stroke()
-    ctx.closePath()
+    ctx.beginPath();
+    ctx.moveTo(0, 20);
+    ctx.lineTo(0, 40);
+    ctx.moveTo(20, 40);
+    ctx.lineTo(20, 60);
+    ctx.moveTo(-20, 40);
+    ctx.lineTo(-20, 60);
+    ctx.stroke();
+    ctx.closePath();
   }
 
-  if (state.pressedKeys.down) {
+  if (ship.pressedKeys.down) {
     ctx.strokeStyle = 'red';
-    ctx.beginPath()
-    ctx.moveTo(20, 0)
-    ctx.lineTo(20, -20)
-    ctx.moveTo(-20, -0)
-    ctx.lineTo(-20, -20)
-    ctx.closePath()
+    ctx.beginPath();
+    ctx.moveTo(20, 0);
+    ctx.lineTo(20, -20);
+    ctx.moveTo(-20, -0);
+    ctx.lineTo(-20, -20);
+    ctx.closePath();
   }
 
-  if (state.pressedKeys.right) {
+  if (ship.pressedKeys.right) {
     ctx.strokeStyle = 'red';
-    ctx.beginPath()
-    ctx.moveTo(0, -40)
-    ctx.lineTo(-20, -40)
-    ctx.moveTo(40, 40)
-    ctx.lineTo(60, 40)
-    ctx.closePath()
+    ctx.beginPath();
+    ctx.moveTo(0, -40);
+    ctx.lineTo(-20, -40);
+    ctx.moveTo(40, 40);
+    ctx.lineTo(60, 40);
+    ctx.closePath();
 
   }
 
-  if (state.pressedKeys.left) {
+  if (ship.pressedKeys.left) {
     ctx.strokeStyle = 'red';
-    ctx.beginPath()
-    ctx.moveTo(0, -40)
-    ctx.lineTo(20, -40)
-    ctx.moveTo(-40, 40)
-    ctx.lineTo(-60, 40)
-    ctx.closePath()
+    ctx.beginPath();
+    ctx.moveTo(0, -40);
+    ctx.lineTo(20, -40);
+    ctx.moveTo(-40, 40);
+    ctx.lineTo(-60, 40);
+    ctx.closePath();
   }
 
   
 
-  ctx.stroke()
-  ctx.restore()
-  drawBullets()
+  ctx.stroke();
+  ctx.restore();
+  
 }
 
 function drawBullets(){
   //ship weapons
 
   for(let i = 0; i < bulletArray.length; i++){
-    ctx.beginPath()
-    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
     ctx.rect(bulletArray[i].xPos, bulletArray[i].yPos, 10, 10);
-    ctx.fill()
-    ctx.closePath()
+    ctx.fill();
+    ctx.closePath();
 
   }
   
@@ -261,15 +266,15 @@ function drawBullets(){
 function loop(timestamp) {
   let progress = timestamp - lastRender;
 
-  update(progress)
-  draw()
+  update(progress);
+  draw();
 
   lastRender = timestamp; //last render is timestamp of last loop
-  window.requestAnimationFrame(loop)
+  window.requestAnimationFrame(loop);
 } // end loop()
 
 let lastRender = 0; // there has been no lastrender when app starts
-window.requestAnimationFrame(loop) // call to get loop started
+window.requestAnimationFrame(loop); // call to get loop started
 
 let keyMap = {
   68: 'right',
@@ -280,12 +285,12 @@ let keyMap = {
 }
 function keydown(event) {
   let key = keyMap[event.keyCode];
-  state.pressedKeys[key] = true;
+  ship.pressedKeys[key] = true;
 }
 function keyup(event) {
   let key = keyMap[event.keyCode];
-  state.pressedKeys[key] = false;
+  ship.pressedKeys[key] = false;
 }
 
-window.addEventListener("keydown", keydown, false)
-window.addEventListener("keyup", keyup, false)
+window.addEventListener("keydown", keydown, false);
+window.addEventListener("keyup", keyup, false);
