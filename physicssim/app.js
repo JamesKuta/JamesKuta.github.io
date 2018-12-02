961abb341eb798c13341c3768b918026e481d3c0
JamesKuta <38857849+JamesKuta@users.noreply.github.com>
2018-12-02 17:08:26 -0600

    Add files via upload

--- physicssim/app.js
+++ physicssim/app.js
@@ -15,6 +15,7 @@ let setUserGravity = document.getElementById("user-gravity");
 let setUserbounciness = document.getElementById("user-bounciness");
 
 let createBallButton = document.getElementById("create");
+let randomButton = document.getElementById("random");
 let resetButton = document.getElementById("reset");
 
 //--Global Variables--//
@@ -32,6 +33,10 @@ let userGravity;
 let userbounciness;
 let groundFriction;
 
+// Random Flag
+let randomSim;
+let randomTimer;
+
 // set initial state of simulation and objects
 function init() {
   screenWidth = window.innerWidth;
@@ -42,15 +47,18 @@ function init() {
   userYVelocity = 0;
   userXVelocityText = `X Velocity = ${userXVelocity}`;
   userYVelocityText = `Y Velocity = ${userYVelocity}`;
-  groundFriction = .99;
+  groundFriction = 0.99;
 
- setUserXVelocity.value = 0;
- setUserYVelocity.value = 10;
- setUserBallRadius.value = 50;
- setUserGravity.value = 1;
- setUserbounciness.value = .8;
+  setUserXVelocity.value = 0;
+  setUserYVelocity.value = 10;
+  setUserBallRadius.value = 50;
+  setUserGravity.value = 1;
+  setUserbounciness.value = 0.8;
 
- balls = [];
+  randomSim = false;
+  randomTimer = 0;
+
+  balls = [];
 }
 
 //ball array
@@ -72,18 +80,71 @@ function getUserYVelocityText() {
 }
 
 function getGravityText() {
-  userGravity = parseFloat(setUserGravity.value,);
+  userGravity = parseFloat(setUserGravity.value);
   return `Gravity = ${userGravity}`;
 }
 
 function getbouncinessText() {
-    userbounciness = parseFloat(setUserbounciness.value);
-    return `Bounciness = ${userbounciness}`;
+  userbounciness = parseFloat(setUserbounciness.value);
+  return `Bounciness = ${userbounciness}`;
 }
 
 function createBall() {
-    balls.push(new Ball(screenWidth /2, 0, userBallRadius, userGravity, userbounciness, userXVelocity, userYVelocity));
-    console.log(balls);
+  balls.push(
+    new Ball(
+      screenWidth / 2,
+      screenHeight / 2,
+      userBallRadius,
+      userGravity,
+      userbounciness,
+      userXVelocity,
+      userYVelocity
+    )
+  );
+  console.log(balls);
+}
+
+function randomSimulation() {
+  let radius = Math.floor(Math.random() * 30) + 5;
+
+  // get a random x velocity between negative and positive value
+  let xVelocity = Math.floor(Math.random() * 30) + 1;
+  if (Math.floor(Math.random() * 2) == 1) {
+  } else {
+    xVelocity *= -1;
+  }
+
+  // get a random y velocity between negative and positive value
+  let yVelocity = Math.floor(Math.random() * 30) + 1;
+  if (Math.floor(Math.random() * 2) == 1) {
+  } else {
+    yVelocity *= -1;
+  }
+
+  //get a random bounciness between .50 and .99
+  let bounciness = (Math.random() * (.50 - .99) + .99).toFixed(2);
+
+  //get a random gravity between .10 and 3
+  //let gravity = (Math.random() * (3 - .1) + .1).toFixed(2);
+  
+
+  if (randomTimer % 30 == 0) {
+    balls.push(
+      new Ball(
+        screenWidth / 2,
+        screenHeight/ 2,
+        radius,
+        userGravity,
+        bounciness,
+        xVelocity,
+        yVelocity
+      )
+    );
+    //console.log(gravity);
+
+    randomSim = true;
+  }
+  randomTimer++;
 }
 
 //--Objects--//
@@ -92,12 +153,12 @@ function Ball(x, y, radius, gravity, bounciness, velocityX, velocityY) {
   this.x = x;
   this.y = y;
   this.radius = radius;
-  this.color = '#00FFFF';
+  this.color = "#FFFFFF";
   this.gravity = gravity;
   this.bounciness = bounciness;
   this.velocityX = velocityX;
   this.velocityY = velocityY;
-  this.friction = .98;
+  this.friction = 0.98;
   this.timeToLive = 0;
   this.lastTimeToLive = 0;
   this.lastX = 0;
@@ -107,52 +168,56 @@ function Ball(x, y, radius, gravity, bounciness, velocityX, velocityY) {
 Ball.prototype.update = function() {
   this.draw();
 
-  if(this.y + this.radius + this.velocityY >= canvas.height){
-    this.velocityY = -this.velocityY  * this.bounciness * this.friction;
+  if (this.y + this.radius + this.velocityY >= canvas.height) {
+    this.velocityY = -this.velocityY * this.bounciness * this.friction;
   } else {
     this.velocityY += this.gravity;
   }
 
-  if(this.x - this.radius + this.velocityX <= 0 || this.x + this.radius + this.velocityX > canvas.width){
-    this.velocityX = -(this.velocityX  * this.bounciness) * this.friction;
-  } 
+  if (
+    this.x - this.radius + this.velocityX <= 0 ||
+    this.x + this.radius + this.velocityX > canvas.width
+  ) {
+    this.velocityX = -(this.velocityX * this.bounciness) * this.friction;
+  }
 
-    this.x += this.velocityX;
-    this.y += this.velocityY;
-    this.timeToLive++;
+  this.x += this.velocityX;
+  this.y += this.velocityY;
+  this.timeToLive++;
 
-    //this.lastX = this.x;
-    //this.lastY = this.y;
-    
+  //this.lastX = this.x;
+  //this.lastY = this.y;
 };
 
 Ball.prototype.draw = function() {
-    c.beginPath();
-    c.fillStyle = this.color;
-    c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    c.fill();
+  c.beginPath();
+  c.strokeStyle = this.color;
+  c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+  c.stroke();
 
-    if(parseFloat(this.x).toFixed(2) == parseFloat(this.lastX).toFixed(2) && parseFloat(this.y).toFixed(3) == parseFloat(this.lastY).toFixed(3)){
-      balls.splice(this, 1);
-      
-    }
+  if (
+    parseFloat(this.x).toFixed(2) == parseFloat(this.lastX).toFixed(2) &&
+    parseFloat(this.y).toFixed(3) == parseFloat(this.lastY).toFixed(3)
+  ) {
+    balls.splice(this, 1);
+  }
 
-    if(this.y == this.lastY && this.timeToLive > 10){
-      this.velocityX *= this.friction; //groundFriction;
-    }
+  if (this.y == this.lastY && this.timeToLive > 10) {
+    this.velocityX *= this.friction; //groundFriction;
+  }
 
-    this.lastY = this.y;
-    //make the balls go away if they are not moving
-    this.lastX = this.x;
-    this.lastY = this.y;
+  this.lastY = this.y;
+  //make the balls go away if they are not moving
+  this.lastX = this.x;
+  this.lastY = this.y;
 
-    //console.log(`currentX: ${parseFloat(this.x).toFixed(2)} lastX: ${parseFloat(this.lastX).toFixed(2)} currentY: ${parseFloat(this.y).toFixed(3)} lastY: ${parseFloat(this.lastY).toFixed(3)}`);
+  //console.log(`currentX: ${parseFloat(this.x).toFixed(2)} lastX: ${parseFloat(this.lastX).toFixed(2)} currentY: ${parseFloat(this.y).toFixed(3)} lastY: ${parseFloat(this.lastY).toFixed(3)}`);
 };
 
 function drawUserSettings() {
-let font = "15px Arial";
-let fontColor = 'white';
-let ySpacing = 20;
+  let font = "15px Arial";
+  let fontColor = "white";
+  let ySpacing = 20;
   c.save();
   c.font = font;
   c.fillStyle = fontColor;
@@ -174,7 +239,7 @@ let ySpacing = 20;
   c.save();
   c.font = font;
   c.fillStyle = fontColor;
-  c.fillText(getGravityText(),0, ySpacing + 60);
+  c.fillText(getGravityText(), 0, ySpacing + 60);
   c.restore();
 
   c.save();
@@ -184,27 +249,34 @@ let ySpacing = 20;
   c.restore();
 }
 
-function drawBalls(){
-
-}
+function drawBalls() {}
 
 function main() {
   requestAnimationFrame(main);
   c.clearRect(0, 0, screenWidth, screenHeight);
 
   drawUserSettings();
-  
-  for(let i = 0; i < balls.length; i++){
+
+  if (randomSim) {
+    randomSimulation();
+  }
+
+  for (let i = 0; i < balls.length; i++) {
     balls[i].update();
   }
-  
 }
 
 //listeners
 window.addEventListener("resize", init);
 
-createBallButton.addEventListener('click', createBall);
-resetButton.addEventListener('click', init);
+createBallButton.addEventListener("click", createBall);
+
+// only allow if random sim is not already running
+if (!randomSim) {
+  randomButton.addEventListener("click", randomSimulation);
+}
+
+resetButton.addEventListener("click", init);
 
 //
 
