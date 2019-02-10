be71a4bf9f7c9204f0a45acdef04483318e8820f
JamesKuta <38857849+JamesKuta@users.noreply.github.com>
2019-02-10 13:38:31 -0600

    Add files via upload

--- BreakoutOOPRedesign/game.js
+++ BreakoutOOPRedesign/game.js
@@ -1,4 +1,4 @@
-let mouse = 
+let mouse =
 {
     x: 0,
     y: 0,
@@ -7,14 +7,13 @@ let mouse =
 let display = new Display(document.getElementById('canvas'));
 let ball = new Ball(canvas.width / 2, canvas.height / 2, 10, 'white');
 let level = new Levels;
-let brick = new Brick(display.gameCanvas.width / 10, display.gameCanvas.height /30, 2, 'blue');
+let brick = new Brick(display.gameCanvas.width / 10, display.gameCanvas.height / 30, 2, 'blue');
 let paddle = new Paddle(mouse.x, display.gameCanvas.height * 0.9, display.gameCanvas.width * 0.15, display.gameCanvas.height / 40, 'white');
 let count = 0;
 let levelBuffer = [];
 let levelCounter = 1;
 
-
-window.onload = function()
+window.onload = function ()
 {
     display.clear();
     loadLevel(level);
@@ -23,139 +22,131 @@ window.onload = function()
 
 function loadLevel(obj) 
 {
-    switch(levelCounter)
+    switch (levelCounter)
     {
         case 1:
-        {
-            levelBuffer = obj.level1
-            break;
-        }
+            {
+                levelBuffer = obj.level1
+                break;
+            }
 
         case 2:
-        {
-            levelBuffer = obj.level2
-            break;
-        }
+            {
+                levelBuffer = obj.level2
+                break;
+            }
     }
 }
 
 function update()
 {
     requestAnimationFrame(update);
-    
+
     display.clear();
-    
+
     worldEdgesCollisionCheck(ball, display);
     ballHitPaddleCollisionCheck(ball, paddle);
     ballHitBrickCollisionCheck(ball, brick, levelBuffer);
-    
+
     ball.move();
 
     movePaddle(paddle);
-    
+
     display.drawCircle(ball);
     display.drawRectangle(paddle);
     //console.log(paddle);
 
     updateGameGrid(levelBuffer, brick);
-    
+
 }
 
-// function checkCollisions(obj1, obj2 ) //worldRight, worldBottom, ballX, ballY 
-// {
-    
-//     worldEdgesCollisionCheck(obj1, obj2);
-//     //ballHitPaddleCollisionCheck(obj1, obj2);
-    
-
-
-// }
-
-function worldEdgesCollisionCheck(obj1, obj2){
-    
-    if((obj1.x - obj1.radius) + obj1.speedX < 0 || (obj1.x + obj1.radius) + obj1.speedX >= obj2.gameCanvas.width) {
-        obj1.speedX = -obj1.speedX;
-        count++;
-        //console.log(count);
-        
-    }
-
-    if(obj1.y < 0 || obj1.y >= obj2.gameCanvas.height) {
-        obj1.speedY *= -1;
-        count++;
-        //console.log(count);
-        
-    }
-}
-
-function ballHitPaddleCollisionCheck(obj1, obj2)
+function worldEdgesCollisionCheck(param_ball, param_display)
 {
-    let topOfPaddle = obj2.y;
-    let leftSideOfPaddle = obj2.x;
-    let rightSideOfPaddle = obj2.x + obj2.width;
-    let centerOfPaddle = leftSideOfPaddle + obj2.width / 2;
-    let setXSpeedFromCenterOffset = obj1.x - centerOfPaddle;
+
+    if (param_ball.x - param_ball.radius < 0 || param_ball.x + param_ball.radius >= param_display.gameCanvas.width)
+    {
+        param_ball.speedX = -param_ball.speedX;
+        count++;
+        //console.log(count);
+
+    }
+
+    if (param_ball.y < 0 || param_ball.y >= param_display.gameCanvas.height)
+    {
+        param_ball.speedY *= -1;
+        count++;
+        //console.log(count);
+
+    }
+}
+
+function ballHitPaddleCollisionCheck(param_ball, param_paddle)
+{
+    let topOfPaddle = param_paddle.y;
+    let leftSideOfPaddle = param_paddle.x;
+    let rightSideOfPaddle = param_paddle.x + param_paddle.width;
+    let centerOfPaddle = leftSideOfPaddle + param_paddle.width / 2;
+    let setXSpeedFromCenterOffset = param_ball.x - centerOfPaddle;
     let offsetSpeedChange = .25;
 
-    if(obj1.x > leftSideOfPaddle &&
-        obj1.x < rightSideOfPaddle&&
-        obj1.y > topOfPaddle)
-        
-    {
-        obj1.speedY = -obj1.speedY;
-        obj1.speedX = setXSpeedFromCenterOffset * offsetSpeedChange;
-        //console.log(obj1.speedX);
+    if (param_ball.x > leftSideOfPaddle &&
+        param_ball.x < rightSideOfPaddle &&
+        param_ball.y > topOfPaddle)
+{
+        param_ball.speedY = -param_ball.speedY;
+        param_ball.speedX = setXSpeedFromCenterOffset * offsetSpeedChange;
+        //console.log(param_ball.speedX);
     }
 }
 
-function currentLevelArrayIndexAtRowAndColumnOfBall(obj1, obj2, obj3) 
+function currentLevelArrayIndexAtRowAndColumnOfBall(param_ball, param_brick, param_levelBuffer) 
 {
-    let getCurrentBallRow = Math.floor(obj1.y / obj2.height);
-    let getCurrentBallColomn = Math.floor(obj1.x / obj2.width);
-    if(getCurrentBallColomn < 0)
-    {
-    console.log("col: " + getCurrentBallColomn + "row: " + getCurrentBallRow);
-    }
-    return getCurrentBallRow * obj3.columns + getCurrentBallColomn;
+    let getCurrentBallRow = Math.floor(param_ball.y / param_brick.height);
+    let getCurrentBallColomn = Math.floor(param_ball.x / param_brick.width);
 
+    if (getCurrentBallRow >= 0 && getCurrentBallRow < param_levelBuffer.rows &&
+        getCurrentBallColomn >= 0 && getCurrentBallColomn < param_levelBuffer.columns) // fix edge wrap
+    {
+        return getCurrentBallRow * param_levelBuffer.columns + getCurrentBallColomn;
+    }
 }
 
-function ballHitBrickCollisionCheck(obj1, obj2, obj3)
+function ballHitBrickCollisionCheck(param_ball, param_brick, param_levelBuffer)
 {
-    let getCurrentLevelArrayIndexAtBall = currentLevelArrayIndexAtRowAndColumnOfBall(obj1, obj2, obj3);
-    
-    if(obj3.grid[getCurrentLevelArrayIndexAtBall] <= obj3.columns * obj3.rows &&
-        obj3.grid[getCurrentLevelArrayIndexAtBall] > 0)
+    let getCurrentLevelArrayIndexAtBall = currentLevelArrayIndexAtRowAndColumnOfBall(param_ball, param_brick, param_levelBuffer);
+
+    if (param_levelBuffer.grid[getCurrentLevelArrayIndexAtBall] <= param_levelBuffer.columns * param_levelBuffer.rows &&
+        param_levelBuffer.grid[getCurrentLevelArrayIndexAtBall] > 0) //Is the index where a brick could be and is there a brick there
     {
-        obj3.grid[getCurrentLevelArrayIndexAtBall] = 0;
-        obj1.speedY = - obj1.speedY;
+        // let ballRowPreviousFrame = Math.floor(param_ball.y / param_brick.height);
+        // let ballColumnPreviousFream = Math.floor(param_ball.x / param_brick.width);
+
+        param_levelBuffer.grid[getCurrentLevelArrayIndexAtBall] = 0;
+        param_ball.speedY = - param_ball.speedY;
     }
-    
 }
 
-function updateGameGrid(obj1, obj2)
+function updateGameGrid(param_levelBuffer, param_brick)
 {
-    for(let row = 0; row < obj1.rows; row++)
+    for (let row = 0; row < param_levelBuffer.rows; row++)
     {
-        for(let col = 0; col < obj1.columns; col++)
+        for (let col = 0; col < param_levelBuffer.columns; col++)
         {
-            if(obj1.grid[row * obj1.columns + col] == 1)
+            if (param_levelBuffer.grid[row * param_levelBuffer.columns + col] == 1)
             {
-                obj2.x = col * obj2.width;
-                obj2.y = row * obj2.height;
-                //console.log(obj2);
+                param_brick.x = col * param_brick.width;
+                param_brick.y = row * param_brick.height;
+                //console.log(param_brick);
 
-                display.drawRectangle(obj2);
+                display.drawRectangle(param_brick);
             }
         }
-
     }
 }
 
-function movePaddle(obj)
+function movePaddle(param_paddle)
 {
-    obj.x = mouse.x - obj.width / 2;
-    
+    param_paddle.x = mouse.x - param_paddle.width / 2;
 }
 
 function onMouseMove(event)
@@ -163,11 +154,9 @@ function onMouseMove(event)
     let rect = display.gameCanvas.getBoundingClientRect();
     mouse.x = event.clientX - rect.left;
     mouse.y = event.clientY - rect.top;
-
 }
 
 window.addEventListener('resize', display.resize);
 window.addEventListener('mousemove', onMouseMove);
 //console.log(brick);
 
-    
