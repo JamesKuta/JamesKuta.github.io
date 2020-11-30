948803ec873694ae96c588003e40a6e9bc5f7bc9
JamesKuta <38857849+JamesKuta@users.noreply.github.com>
2020-11-29 19:46:41 -0600

    Add files via upload

--- sweetswap/js/Grid.js
+++ sweetswap/js/Grid.js
@@ -4,7 +4,7 @@ class Grid
     {
         //reference to self
         let grid = this;
-        
+
         // Set drawing canvas properties
         grid.canvas = canvas;
         grid.context = grid.canvas.getContext("2d");
@@ -29,7 +29,7 @@ class Grid
 
         //For storing matches
         grid.matches = []; //{cells}
-        
+
         //For storing valide moves
         grid.validMoves = []; //{cells}
 
@@ -45,13 +45,13 @@ class Grid
 
         let finished = false;
         //Need to create a grid with no 3 in a row, but at least one possible move
-        while(!finished)
+        while (!finished)
         {
             //Generate cells to fill the grid randomly
-            for(let row = 0; row < grid.rows; row++)
+            for (let row = 0; row < grid.rows; row++)
             {
                 grid.cells[row] = [];
-                for(let col = 0; col < grid.cols; col++)
+                for (let col = 0; col < grid.cols; col++)
                 {
                     let randomType = Utilities.RandomIntegerBetweenMinMax(grid.cellImg.length);
                     let cellImg = grid.cellImg[randomType];
@@ -60,7 +60,7 @@ class Grid
                     grid.cells[row][col] = cell;
                 }
             }
-            
+
             grid.HandleMatches();
 
             //grid.FindValidSwaps();
@@ -97,36 +97,38 @@ class Grid
         grid.matches = [];
 
         //Horizontal Matches
-        for(let row = 0; row < grid.rows; row++)
+        for (let row = 0; row < grid.rows; row++)
         {
             let matchesCount = 1;
 
-            for(let col = 0; col < grid.cols; col++)
+            for (let col = 0; col < grid.cols; col++)
             {
                 let checkMatches = false;
 
                 if (col == grid.cols - 1)
                 {
                     checkMatches = true;
-                }else
+                } else
                 {
-                    if(grid.cells[row][col].matchNum == grid.cells[row][col+1].matchNum)
+                    if (grid.cells[row][col].matchNum == grid.cells[row][col + 1].matchNum)
                     {
                         matchesCount += 1;
-                    }else
+                    } else
                     {
                         checkMatches = true;
                     }
                 }
 
-                if(checkMatches)
+                if (checkMatches)
                 {
-                    if(matchesCount >= 3)
+                    if (matchesCount >= 3)
                     {
-                        grid.matches.push({colNum: col + 1 - matchesCount,
-                                            rowNum: row,
-                                            length: matchesCount,
-                                            horizontal: true});
+                        grid.matches.push({
+                            colNum: col + 1 - matchesCount,
+                            rowNum: row,
+                            length: matchesCount,
+                            horizontal: true
+                        });
                     }
                     matchesCount = 1;
                 }
@@ -134,36 +136,38 @@ class Grid
         }
 
         //Vertical Matches
-        for(let col = 0; col < grid.cols; col++)
+        for (let col = 0; col < grid.cols; col++)
         {
             let matchesCount = 1;
 
-            for(let row = 0; row < grid.rows; row++)
+            for (let row = 0; row < grid.rows; row++)
             {
                 let checkMatches = false;
 
                 if (row == grid.rows - 1)
                 {
                     checkMatches = true;
-                }else
+                } else
                 {
-                    if(grid.cells[row][col].matchNum == grid.cells[row + 1][col].matchNum)
+                    if (grid.cells[row][col].matchNum == grid.cells[row + 1][col].matchNum)
                     {
                         matchesCount += 1;
-                    }else
+                    } else
                     {
                         checkMatches = true;
                     }
                 }
 
-                if(checkMatches)
+                if (checkMatches)
                 {
-                    if(matchesCount >= 3)
+                    if (matchesCount >= 3)
                     {
-                        grid.matches.push({colNum: col,
-                                            rowNum: row + 1 - matchesCount,
-                                            length: matchesCount,
-                                            horizontal: false});
+                        grid.matches.push({
+                            colNum: col,
+                            rowNum: row + 1 - matchesCount,
+                            length: matchesCount,
+                            horizontal: false
+                        });
                     }
                     matchesCount = 1;
                 }
@@ -175,20 +179,20 @@ class Grid
     {
         let grid = this;
 
-        grid.matches.forEach(function(matches)
+        grid.matches.forEach(function (matches)
         {
-            if(matches.horizontal == true)
+            if (matches.horizontal == true)
             {
-                for(let i = 0; i < matches.length; i++)
+                for (let i = 0; i < matches.length; i++)
                 {
                     grid.cells[matches.rowNum][matches.colNum + i].empty = true;
                 }
-                
+
             }
 
-            if(matches.horizontal == false)
+            if (matches.horizontal == false)
             {
-                for(let i = 0; i < matches.length; i++)
+                for (let i = 0; i < matches.length; i++)
                 {
                     grid.cells[matches.rowNum + i][matches.colNum].empty = true;
                 }
@@ -201,18 +205,24 @@ class Grid
         let grid = this;
 
         //start with the second to last row and move up
-        for(let row = grid.rows - 2; row > 0; row--)
+        for (let row = grid.rows - 2; row > 0; row--)
         {
-            for(let col = 0; col < grid.cols; col++)
+            for (let col = 0; col < grid.cols; col++)
             {
-                if(grid.cells[row][col].empty == false && grid.cells[row + 1][col].empty == true)
+                let countEmptyBelow = true;
+                let counter = 1;
+                while (countEmptyBelow && row + counter < grid.rows)
                 {
-                    console.log(row + 1 + ", " + [col]);
-                    
-                    
-                    grid.cells[row][col].moveDownCount += 1;
-                    console.log(grid.cells[row][col].moveDownCount);
-                    //figure out how many down to move create a movedowncount
+                    if (grid.cells[row][col].empty == false && grid.cells[row + counter][col].empty == true)
+                    {
+                        grid.cells[row][col].moveDownCount += 1;
+                        counter++;
+                        console.log(grid.cells[row][col].moveDownCount);
+                        //figure out how many down to move create a movedowncount
+                    } else
+                    {
+                        countEmptyBelow = false;
+                    }
                 }
             }
         }
@@ -229,6 +239,6 @@ class Grid
         let grid = this;
 
         grid.context.fillStyle = "rgba(0, 0, 100, 0.2)";
-        grid.context.fillRect(grid.x, grid.y, grid.width, grid.height);    
+        grid.context.fillRect(grid.x, grid.y, grid.width, grid.height);
     }
 }
