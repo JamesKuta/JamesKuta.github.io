8a4138f536d5db4631d547c16feab3f2de172384
JamesKuta <38857849+JamesKuta@users.noreply.github.com>
2021-01-17 07:56:13 -0600

    Add files via upload

---
+++ sugarcrushgame/js/levels.js
@@ -0,0 +1,36 @@
+levels =
+
+[
+    {
+        rows: 9,
+        cols: 9,
+        grid: 
+        [
+            [null,null,null,null,null,null,null,null,null],
+            [null,null,null,null,null,null,null,null,null],
+            [null,null,null,null,null,null,null,null,null],
+            [null,null,null,null,null,null,null,null,null],
+            [null,null,null,null,null,null,null,null,null],
+            [null,null,null,null,null,null,null,null,null],
+            [null,null,null,null,null,null,null,null,null],
+            [null,null,null,null,null,null,null,null,null],
+            [null,null,null,null,null,null,null,null,null]
+        ]
+    },
+    {
+        rows: 9,
+        cols: 9,
+        grid: 
+        [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ]
+    }
+]
---
+++ sugarcrushgame/js/main.js
@@ -0,0 +1,29 @@
+/*
+Idea for name: Sweet Swap
+
+Thrice Match Game (Sweet Swap)
+Author: James Kuta
+November 2020
+
+Free to use Graphics Assets provided by:
+Author: Lumimae (http://thecandyjam.com)
+Source: https://opengameart.org/content/candy-pack-1
+*/
+
+window.onload = function ()
+{
+    //Grab the Canvas Element from HTML
+    let canvas = document.getElementById("canvas");
+
+    //Create the Game Object
+    let SugarCrush = new Game(canvas);
+
+    //Loading Images
+    //SugarCrush.loadImages();
+    
+    //Loading Audio
+    //SugarCrush.loadAudio();
+
+    //Start the Game
+    //SugarCrush.Start();
+}
